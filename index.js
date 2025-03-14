b959eba2521543042d27fd88233c3963b4ed2e57
Stauber Thomas <thomas.stauber@student-cs.fr>
2025-03-14 16:47:18 +0100

    added json parsing

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var app = express(); //instanciation d'une application Express
 const cors = require('cors');  // Import CORS middleware
 // Pour s'assurer que l'on peut faire des appels AJAX au serveur
 app.use(cors());
+app.use(express.json());  
+app.use(express.urlencoded({ extended: true }));  
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
